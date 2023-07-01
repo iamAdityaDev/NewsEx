03b9aacbdb3ff20a3215a9924dcc502558b8daa2
Aditya Dev <iamadityadevpro@gmail.com>
2023-07-01 15:48:01 +0530

    hibsds

--- src/component/news.js
+++ src/component/news.js
@@ -1,12 +1,30 @@
 import "./news.css";
 import React, { useEffect, useState } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
 import NewsUpdate from "./news_update";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+// ... (imports remain the same)
 
 const News = (props) => {
-  const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState({});
   const [page, setPage] = useState(1);
-  const [error, setError] = useState(false);
+  const [totalResults, setTotalResults] = useState(0);
+
+  const updateNews = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=969db4cd0c684fdc9b4fac646850d457&page=${page}&pageSize=10&category=${props.category}`;
+    let data = await fetch(url);
+    let parsed_data = await data.json();
+
+    // Merge the new articles with the existing ones using the 'url' property as the key
+    setArticles((prevArticles) => {
+      return { ...prevArticles, ...parsed_data.articles.reduce((obj, article) => {
+        obj[article.url] = article;
+        return obj;
+      }, {}) };
+    });
+
+    setTotalResults(parsed_data.totalResults);
+  };
 
   useEffect(() => {
     document.title = `NewsEx - ${props.category}`;
@@ -14,62 +32,53 @@ const News = (props) => {
     // eslint-disable-next-line
   }, [props.category]);
 
-  const updateNews = async () => {
-    try {
-      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=969db4cd0c684fdc9b4fac646850d457&page=${page}&pageSize=10&category=${props.category}`;
-      const data = await fetch(url);
-      const parsed_data = await data.json();
-      setArticles((prevArticles) => [...prevArticles, ...parsed_data.articles]);
-      setError(false);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      setError(true);
-    }
+  const fetchMoreData = async () => {
+    const nextPage = page + 1;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=969db4cd0c684fdc9b4fac646850d457&page=${nextPage}&pageSize=10&category=${props.category}`;
+    let data = await fetch(url);
+    let parsed_data = await data.json();
+
+    // Merge the new articles with the existing ones using the 'url' property as the key
+    setArticles((prevArticles) => {
+      return { ...prevArticles, ...parsed_data.articles.reduce((obj, article) => {
+        obj[article.url] = article;
+        return obj;
+      }, {}) };
+    });
+
+    setTotalResults(parsed_data.totalResults);
+    setPage(nextPage);
   };
 
-  const fetchMoreData = async () => {
-    try {
-      const nextPage = page + 1;
-      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=969db4cd0c684fdc9b4fac646850d457&page=${nextPage}&pageSize=10&category=${props.category}`;
-      const data = await fetch(url);
-      const parsed_data = await data.json();
-      setArticles((prevArticles) => [...prevArticles, ...parsed_data.articles]);
-      setPage(nextPage);
-    } catch (error) {
-      console.error("Error fetching more data:", error);
-      setError(true);
-    }
-  };
+  const uniqueArticles = Object.values(articles);
 
   return (
     <>
       <p className="headlines">NewsEx - Top Headlines</p>
-      {error ? (
-        <div className="error-message">Error fetching news data. Please try again later.</div>
-      ) : (
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== 0} // Adjusted the condition here
-        >
-          <div className="container">
-            {articles.map((element, index) => (
-              <NewsUpdate
-                key={index}
-                news_url={element.url}
-                name={element.source.name}
-                author={element.author}
-                date={element.publishedAt}
-                image={element.urlToImage}
-                title={element.title ? element.title.slice(0, 75) : ""}
-                description={
-                  element.description ? element.description : element.title.slice(0, 90)
-                }
-              />
-            ))}
-          </div>
-        </InfiniteScroll>
-      )}
+      <InfiniteScroll
+        dataLength={uniqueArticles.length}
+        next={fetchMoreData}
+        hasMore={uniqueArticles.length !== totalResults}
+      >
+        <div className="container">
+          {uniqueArticles.map((element, index) => (
+            <NewsUpdate
+              key={index}
+              news_url={element.url}
+              name={element.source.name}
+              author={element.author}
+              date={element.publishedAt}
+              image={element.urlToImage}
+              title={element.title ? element.title.slice(0, 75) : ""}
+              description={
+                element.description
+                  ? element.description
+                  : element.title.slice(0, 90)
+              }
+            />
+          ))}
+        </div>
+      </InfiniteScroll>
     </>
   );
 };
